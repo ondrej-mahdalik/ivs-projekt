6840084565cf5cd7908e0aecf3998ec5ba311684
Ondřej Mahdalík <49830945+ondrej-mahdalik@users.noreply.github.com>
2021-02-17 21:02:07 +0100

    Jasnější rozdělení GUI a matematické části. Matematická část nakonec udělána univerzálně - měla by jít zkompilovat i pro jiné OS Nějaké základní funkce v matematické knihovně. Primitivní ukázka použití matematické knihovny v GUI.

---
+++ Math/Math.cs
@@ -0,0 +1,62 @@
+﻿namespace Math
+{
+    public class MathClass
+    {
+        /// <summary>
+        /// Processes string input, performs all operations in correct order.
+        /// </summary>
+        /// <param name="input">Text to proccess</param>
+        /// <returns>Final result</returns>
+        public static double FromString(string input)
+        {
+            // TODO
+            return 0.0;
+        }
+
+        /// <summary>
+        /// Adds one number to another. (Ano, je to zbytečná, ale ať jsou všechny operace v této knihovně a nic v GUI).
+        /// </summary>
+        /// <param name="inputA">Number to proccess</param>
+        /// <param name="inputB">Number to proccess</param>
+        /// <returns>Result</returns>
+        public static double Add(double inputA, double inputB)
+        {
+            return inputA + inputB;
+        }
+
+        /// <summary>
+        /// Substracts one number from another.
+        /// </summary>
+        /// <param name="inputA">Number to subtract from</param>
+        /// <param name="inputB">Number to </param>
+        /// <returns>Result</returns>
+        public static double Subtract(double inputA, double inputB)
+        {
+            return inputA - inputB;
+        }
+
+        /// <summary>
+        /// Divides one number from another.
+        /// </summary>
+        /// <param name="inputA"></param>
+        /// <param name="inputB"></param>
+        /// <returns>Result</returns>
+        public static double Divide(double inputA, double inputB)
+        {
+            return inputA / inputB;
+        }
+
+        /// <summary>
+        /// Multiplies one number by another.
+        /// </summary>
+        /// <param name="inputA"></param>
+        /// <param name="inputB"></param>
+        /// <returns>Result</returns>
+        public static double Multiply(double inputA, double inputB)
+        {
+            return inputA * inputB;
+        }
+
+        // TODO - dalsi funkce atd.
+    }
+}
