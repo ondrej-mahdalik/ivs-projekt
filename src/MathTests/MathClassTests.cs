20a75e25702c00e34dcd50a127c5692d71a93754
honzav23 <79053958+honzav23@users.noreply.github.com>
2021-03-03 18:12:38 +0100

    Přidány základní testy
    Jen na zkoušku, že to zhruba funguje

---
+++ src/MathTests/MathClassTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Math.Tests
+{
+        [TestClass()]
+        public class MathClassTests
+        {
+            double num1 = 4;
+            double num2 = 8;
+            [TestMethod()]
+            public void AdditionTest()
+            {
+                double result = MathClass.Add(num1, num2);
+                double expected = 12;
+                Assert.AreEqual(result, expected);
+            }
+            [TestMethod()]
+            public void SubtractionTest()
+            {
+                double result = MathClass.Subtract(num2, num1);
+                double expected = 4;
+                Assert.AreEqual(result, expected);
+            }
+            [TestMethod()]
+            public void DivisionTest() 
+            {
+                double result = MathClass.Divide(num2, num1);
+                double expected = 2;
+                Assert.AreEqual(result, expected);
+                Assert.ThrowsException<ArithmeticException>(() => MathClass.Divide(num1, 0));
+            }
+        }
+}
