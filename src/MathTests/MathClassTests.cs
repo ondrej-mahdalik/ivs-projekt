6a1ce63c80c40d534051ecc240491199c438ed81
honzav23 <janvaclavik@valachnet.cz>
2021-03-09 16:54:35 +0100

    Přidány testy pro matematické operace - bez matematické knihovny

--- src/MathTests/MathClassTests.cs
+++ src/MathTests/MathClassTests.cs
@@ -121,10 +121,6 @@ namespace Math.Tests
             for (int i = 0; i < firstNums.Length; i++) {
                 Assert.AreEqual(results[i], (MathClass.Logarithm(firstNums[i])));
             }
-            Assert.ThrowsException<ArithmeticException>(() => MathClass.Logarithm(0));
-            Assert.ThrowsException<ArithmeticException>(() => MathClass.Logarithm(-1));
-            Assert.ThrowsException<ArithmeticException>(() => MathClass.Logarithm(-1500.5654));
-
         }
 
     }
