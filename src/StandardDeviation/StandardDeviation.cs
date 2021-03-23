5a4928d5c22c49f08b2c94097cb54c4a200cfa08
honzav23 <janvaclavik@valachnet.cz>
2021-03-23 21:19:25 +0100

    Rozdělení výpočtu do funkcí, připsány komentáře

--- src/StandardDeviation/StandardDeviation.cs
+++ src/StandardDeviation/StandardDeviation.cs
@@ -2,49 +2,58 @@
 using Math;
 namespace StandardDeviation
 {
-    class StandardDeviation
+    public class StandardDeviationClass
     {
         /// <summary>
-        /// Counts the standard deviation.
+        /// Converts the string of numbers to an array of numbers.
         /// </summary>
-        /// <param name="sum">Total summary of all numbers</param>
-        /// <param name="numbers">Array of given numbers to count the standard deviation from</param>
-        /// <returns>Result</returns>
-        static double CountStandardDeviation(double sum, double[] numbers)
+        /// <param name="input">Input to parse.</param>
+        /// <returns>Array of doubles.</returns>
+        public static double[] GetNumbersFromString(string input)
         {
-            double average = sum / numbers.Length;
-            double poweredSum = 0;
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                poweredSum += MathClass.Power(numbers[i], 2);
-            }
-            double sumOfDeviations = poweredSum - numbers.Length * MathClass.Power(average, 2);
-            double result = MathClass.Root(sumOfDeviations / (numbers.Length - 1), 2);
-            return result;
-        }
-        static void Main(string[] args)
-        {
-            string a;
-            string[] cisla;
-            a = Console.ReadLine();
-            cisla = a.Split(' ', '\n');
-            double[] numbers = new double[cisla.Length];
-            double sum = 0;
-            //Checking whether the correct input is given.
+            string[] splitInput = input.Split(' ', '\n');
+            double[] numbers = new double[splitInput.Length];
+          
+            //Checking whether the correct input is given
             try
             {
-                for (int i = 0; i < cisla.Length; i++)
+                for (int i = 0; i < splitInput.Length; i++)
                 {
-                    numbers[i] = Double.Parse(cisla[i]);
-                    sum += numbers[i];
+                    numbers[i] = Double.Parse(splitInput[i]);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+                System.Environment.Exit(1);
             }
-
-            Console.WriteLine(CountStandardDeviation(sum, numbers));
+            return numbers;
+        }
+        /// <summary>
+        /// Counts the standard deviation.
+        /// </summary>
+        /// <param name="input">Data to count the standard deviation from.</param>
+        /// <returns>Standard deviation.</returns>
+        public static double CountStandardDeviation(string input)
+        {
+            double[] numbers = GetNumbersFromString(input);
+            double sum = 0;
+            double poweredSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+                poweredSum += MathClass.Power(numbers[i], 2);
+            }
+            double average = sum / numbers.Length;
+            double sumOfDeviations = poweredSum - numbers.Length * MathClass.Power(average, 2);
+            double result = MathClass.Root(sumOfDeviations / (numbers.Length - 1), 2);
+            return result;
+        }
+        public static void Main(string[] args)
+        {
+            string input;
+            input = Console.ReadLine();
+            Console.WriteLine(CountStandardDeviation(input));
         }
     }
 }
