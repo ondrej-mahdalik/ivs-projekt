6a33d3e2f82bad898384bd2aff87fd9f042e77ae
Jan Václavík <79053958+honzav23@users.noreply.github.com>
2021-03-26 14:56:25 +0100

    Směrodatná odchylka + testy na směrodatnou odchylku (#33)
    * Výpočet výběrové směrodatné odchylky, zatím bez testů.
    
    * Změna umístění programu StandardDeviation
    
    * Rozdělení výpočtu do funkcí, připsány komentáře
    
    * StandardDeviation bez obj složek
    
    * Přidána směrodatná odchylka s úpravou výjimek
    
    * Úprava struktury programu
    
    * Přidáno více oddělovačů, změna struktury programu
    
    * Přidáni testy na směrodatnou odchylku

--- src/StandardDeviation/StandardDeviation.cs
+++ src/StandardDeviation/StandardDeviation.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using Math;
+
 namespace StandardDeviation
 {
     public class StandardDeviationClass
@@ -11,34 +12,34 @@ namespace StandardDeviation
         /// <returns>Array of doubles.</returns>
         public static double[] GetNumbersFromString(string input)
         {
-            string[] splitInput = input.Split(' ', '\n');
+            string[] splitInput = input.Split(' ', '\n', '\t');
             double[] numbers = new double[splitInput.Length];
 
             //Checking whether the correct input is given
             try
             {
                 for (int i = 0; i < splitInput.Length; i++)
-                {
                     numbers[i] = Double.Parse(splitInput[i]);
-                }
             }
-            catch (Exception e)
+            catch (FormatException)
             {
-                Console.WriteLine(e.Message);
-                System.Environment.Exit(1);
+                throw new FormatException();
             }
             return numbers;
         }
+      
         /// <summary>
         /// Counts the standard deviation.
         /// </summary>
         /// <param name="input">Data to count the standard deviation from.</param>
         /// <returns>Standard deviation.</returns>
-        public static double CountStandardDeviation(string input)
+        public static double CountStandardDeviation(double[] numbers)
         {
-            double[] numbers = GetNumbersFromString(input);
             double sum = 0;
             double poweredSum = 0;
+            if (numbers.Length == 1)
+                throw new DivideByZeroException();
+
             for (int i = 0; i < numbers.Length; i++)
             {
                 sum += numbers[i];
@@ -53,7 +54,8 @@ namespace StandardDeviation
         {
             string input;
             input = Console.ReadLine();
-            Console.WriteLine(CountStandardDeviation(input));
+            double[] numbers = GetNumbersFromString(input);
+            Console.WriteLine(CountStandardDeviation(numbers));
         }
     }
 }
---
+++ src/StandardDeviationTests/StandardDeviationClassTests.cs
@@ -0,0 +1,67 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StandardDeviation;
+using Math;
+using System;
+namespace StandardDeviationTests
+{
+    [TestClass()]
+    public class StandardDeviationClassTests
+    {
+        [TestMethod()]
+        public void ParseInputTest()
+        {
+            //Testing basic parsing
+            string input = "1 2 3 4 5 6 7 8 9 10";
+            double[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            CollectionAssert.AreEqual(expected, StandardDeviationClass.GetNumbersFromString(input));
+            input = "455.564 -26.4871345 -0.974 410001.05 -248764.145";
+            double[] expected2 = { 455.564, -26.4871345, -0.974, 410001.05, -248764.145 };
+            CollectionAssert.AreEqual(expected2, StandardDeviationClass.GetNumbersFromString(input));
+
+            //Testing field separators
+            input = "1\t2\t3\t4\t5\t6\t7\t8\t9\t10";
+            CollectionAssert.AreEqual(expected, StandardDeviationClass.GetNumbersFromString(input));
+            input = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+            CollectionAssert.AreEqual(expected, StandardDeviationClass.GetNumbersFromString(input));
+            input = "1\t2\n3 4\n5\t6 7 8\n9\t10";
+            CollectionAssert.AreEqual(expected, StandardDeviationClass.GetNumbersFromString(input));
+
+            //Testing exceptions
+            input = "asd asd";
+            Assert.ThrowsException<FormatException>(() => StandardDeviationClass.GetNumbersFromString(input));
+            input = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10";
+            Assert.ThrowsException<FormatException>(() => StandardDeviationClass.GetNumbersFromString(input));
+            input = "";
+            Assert.ThrowsException<FormatException>(() => StandardDeviationClass.GetNumbersFromString(input));
+        }
+        [TestMethod()]
+        public void CountDeviationTest() 
+        {
+            double[] basicInput = { 25.062, -97.542, 129.8721, 17546.4523, -500.07, -780.45 };
+            double expected = 7271.254131903278;
+
+            Assert.AreEqual(expected, StandardDeviationClass.CountStandardDeviation(basicInput));
+
+            double[] inputs = new double[1000];
+            double cnt = 1;
+            for (int i = 0; i < inputs.Length; i++) {
+                inputs[i] = cnt++;
+            }
+            expected = 288.81943609574938;
+            Assert.AreEqual(expected, StandardDeviationClass.CountStandardDeviation(inputs));
+
+            double[] inputs2 = new double[10000];
+            cnt = -5568.458;
+            for (int i = 0; i < inputs2.Length; i++) {
+                inputs2[i] = cnt + cnt / 7;
+                cnt++;
+            }
+            expected = 3298.9430187987136;
+            Assert.AreEqual(expected, StandardDeviationClass.CountStandardDeviation(inputs2));
+
+            double[] onlyOneInput = new double[1];
+            onlyOneInput[0] = -2445.56756;
+            Assert.ThrowsException<DivideByZeroException>(() => StandardDeviationClass.CountStandardDeviation(onlyOneInput));
+        }
+    }
+}
