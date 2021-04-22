550fdbbc52c2ad28227118f7f8518fd6573eff5e
Ondřej Mahdalík <xmahda14@vutbr.cz>
2021-04-22 12:22:51 +0200

    Opravy chyb v textu, odstraění nepotřebných závislostí.

--- src/GUI/HelpWindow.Designer.cs
+++ src/GUI/HelpWindow.Designer.cs
@@ -61,7 +61,7 @@ namespace GUI
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(618, 291);
+            this.tabPage1.Size = new System.Drawing.Size(618, 317);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Basic Controls";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -73,7 +73,7 @@ namespace GUI
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(618, 291);
+            this.tabPage2.Size = new System.Drawing.Size(618, 317);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Math Functions";
             this.tabPage2.UseVisualStyleBackColor = true;
