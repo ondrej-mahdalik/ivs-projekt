63d3e0aabb84d11c105fa1b624938222ee10f531
Ondřej Mahdalík <xmahda14@vutbr.cz>
2021-04-23 15:15:43 +0200

    Popis namespace

--- src/GUI/MainWindow.cs
+++ src/GUI/MainWindow.cs
@@ -4,6 +4,9 @@ using System.Globalization;
 using System.Windows.Forms;
 using Math;
 
+/// <summary>
+///     User-friendly graphic interface for the Math library.
+/// </summary>
 namespace GUI
 {
     /// <summary>
