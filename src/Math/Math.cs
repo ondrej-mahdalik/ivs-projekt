c42be9dc22090eb550810d56f811be0332777d1b
Ondřej Mahdalík <xmahda14@vutbr.cz>
2021-04-23 15:10:34 +0200

    Revertování "Odstranění komentáře u namespace"
    Toto odstraní commit dda6df332d034b04324ccf3ff64b389316caf03f.

--- src/Math/Math.cs
+++ src/Math/Math.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Globalization;
 
+/// <summary>
+/// Provides mathematical operations and functions.
+/// </summary>
 namespace Math
 {
     /// <summary>
